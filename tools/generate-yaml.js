ec198f3f8b2236e4830272c570abcb3ce5732f92
Dan Burzo <danburzo@gmail.com>
2013-11-02 16:28:33 +0200

    Added more fields to generated yaml

--- tools/generate-yaml.js
+++ tools/generate-yaml.js
@@ -7,7 +7,14 @@ buildings.features.forEach(function(feature) {
 	var id = feature.properties.id.replace('way/', '');
 	var output_file = output_dir + id + '.yaml';
 	var data = 'id: ' + id + '\n';
-	data+= 'name: ' + feature.properties.name + '\n';
+	data += 'name: ' + feature.properties.name + '\n';
+	data += 'year: \n';
+	data += 'architect: \n';
+	data += 'description: |\n  \n';
+	data += 'photos: \n';
+	data += '  -\n';
+	data += '  -\n';
+	data += '  -\n';
 	if (!fs.existsSync(output_file)) {
 		fs.outputFile(output_file, data);
 	}
