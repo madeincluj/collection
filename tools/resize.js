3b48eb0aa97cea11a2d73bdedf10c1d781860839
Dan Burzo <danburzo@gmail.com>
2013-11-03 02:56:07 +0200

    Added geolocation to some images; scraped clujnapoca.ro; added config file for resize tool

--- tools/resize.js
+++ tools/resize.js
@@ -1,40 +1,45 @@
 var fs = require('fs-extra');
 var walk = require('walk');
 var im = require('imagemagick');
+var yaml = require('js-yaml');
 
-var original_dir = '../s3/collection/dg/original';
+var folders = yaml.safeLoad(fs.readFileSync('tools/resize.yaml', 'utf8')).folders;
 
-var walker = walk.walk(original_dir, {
-	followLinks: false
-});
+folders.forEach(function(folder) {
+	var original_dir = process.cwd() + '/' + folder + '/original';
+	console.log(original_dir);
+	var walker = walk.walk(original_dir, {
+		followLinks: false
+	});
 
-walker.on('file', function(root, filestats, next) {
+	walker.on('file', function(root, filestats, next) {
 
-	console.log('Resizing file: ' + root + '/' + filestats.name);
+		console.log('Resizing file: ' + root + '/' + filestats.name);
 
-	var thumb_dir = root.replace('original', 'thumb');
-	var large_dir = root.replace('original', 'large'); 
-	fs.mkdirsSync(thumb_dir);
-	fs.mkdirsSync(large_dir);
-	im.resize({
-		srcPath: root + '/' + filestats.name,
-		dstPath: thumb_dir + '/' + filestats.name,
-		quality: 90,
-		width: 200,
-		height: 200,
-		sharpening: 0.1
-	}, function(err, stdout) {
-		if (err) throw err;
+		var thumb_dir = root.replace('original', 'thumb');
+		var large_dir = root.replace('original', 'large'); 
+		fs.mkdirsSync(thumb_dir);
+		fs.mkdirsSync(large_dir);
 		im.resize({
 			srcPath: root + '/' + filestats.name,
-			dstPath: large_dir + '/' + filestats.name,
+			dstPath: thumb_dir + '/' + filestats.name,
 			quality: 90,
-			width: 800,
-			height: 600,
+			width: 200,
+			height: 200,
 			sharpening: 0.1
-		}, function(err) {
+		}, function(err, stdout) {
 			if (err) throw err;
-			next();
+			im.resize({
+				srcPath: root + '/' + filestats.name,
+				dstPath: large_dir + '/' + filestats.name,
+				quality: 90,
+				width: 800,
+				height: 600,
+				sharpening: 0.1
+			}, function(err) {
+				if (err) throw err;
+				next();
+			});
 		});
 	});
 });
