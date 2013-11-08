d6d2286a0ceb1176464c175742474c49b6edb04e
Dan Burzo <dburzo@sdl.com>
2013-11-08 14:59:03 +0200

    Added monument metadata

---
+++ tools/monuments.js
@@ -0,0 +1,36 @@
+var fs = require('fs-extra');
+var yaml = require('js-yaml');
+
+var yaml_dir = 'monuments/metadata/';
+var yaml_files = fs.readdirSync(yaml_dir);
+var output_dir = 'monuments/json/metadata/';
+var geojson_output = 'monuments/json/monuments.json';
+
+var geojson = {
+	type: 'FeatureCollection',
+	features: []
+};
+
+yaml_files.forEach(function(filepath) {
+	var file = fs.readFileSync(yaml_dir + filepath, 'utf8');
+	var content = yaml.safeLoad(file);
+
+	var feature = {
+		properties: {
+			name: content.name,
+			id: content.id
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: content.coordinates ? content.coordinates.split(',').map(function(part) {
+				return part.trim();
+			}) : [0, 0]
+		}
+	};
+
+	geojson.features.push(feature);
+
+	var output = JSON.stringify(content, null, 2);
+	fs.outputFile(output_dir + content.id + '.json', output);
+	fs.outputFile(geojson_output, JSON.stringify(geojson, null, 2));
+});
